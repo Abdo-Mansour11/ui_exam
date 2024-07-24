807c774acb5fea052e6d2b95008caa9e33d33156
AbdoMansour <abdo.bedo2004@gmail.com>
2024-07-24 18:31:28 +0300

    second screen

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home_screen extends StatelessWidget {
+  const Home_screen({super.key});
+static const String routeName ='moody';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
