c90d9c08a15026c7c6a5bb2357b2b5a017d6b9d3
AbdoMansour <abdo.bedo2004@gmail.com>
2024-07-24 19:57:14 +0300

    second screen

--- lib/home.dart
+++ lib/home.dart
@@ -6,8 +6,23 @@ static const String routeName ='moody';
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(
-        leading: Image.asset("assets/images/logo.png"),),
+
+
+      body:
+      Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 20 ,left:30 ,right: 30),
+            color: Colors.red,
+            child: Row(
+              children: [
+                Image.asset(height: 55,"assets/images/logo.png",),
+              Icon(Icons.)
+              ],
+            ),
+          ),
+        ],
+      )
 
 
     );
